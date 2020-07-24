45940cfd03c447f3ac13c2f85e328bde37c7ab6e
osaka911 <osaka911@gmail.com>
2020-07-24 14:04:49 +0700

    update example

--- app/src/main/java/tv/wiinvent/android/wiinvent_android_sample_java/MainActivity.java
+++ app/src/main/java/tv/wiinvent/android/wiinvent_android_sample_java/MainActivity.java
@@ -1,35 +1,74 @@
 package tv.wiinvent.android.wiinvent_android_sample_java;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.ContextCompat;
 
+import android.content.ComponentName;
+import android.content.pm.ActivityInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.media.session.MediaSessionCompat;
+import android.support.v4.media.session.PlaybackStateCompat;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 
-import com.google.android.exoplayer.ExoPlayer;
+import com.google.android.exoplayer2.C;
+import com.google.android.exoplayer2.Player;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.ConcatenatingMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.ProgressiveMediaSource;
+import com.google.android.exoplayer2.source.dash.DashMediaSource;
+import com.google.android.exoplayer2.source.hls.HlsMediaSource;
+import com.google.android.exoplayer2.source.smoothstreaming.SsMediaSource;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import kotlin.jvm.internal.MagicApiIntrinsics;
 import tv.wiinvent.wiinventsdk.OverlayManager;
 import tv.wiinvent.wiinventsdk.interfaces.DefaultOverlayEventListener;
 import tv.wiinvent.wiinventsdk.interfaces.PlayerChangeListener;
 import tv.wiinvent.wiinventsdk.interfaces.UserActionListener;
 import tv.wiinvent.wiinventsdk.models.ConfigData;
 import tv.wiinvent.wiinventsdk.models.OverlayData;
+import tv.wiinvent.wiinventsdk.models.StreamSource;
+import tv.wiinvent.wiinventsdk.ui.OverlayView;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String SAMPLE_ACCOUNT_ID = "1";
-    public static final String SAMPLE_CHANNEL_ID = "1";
-    public static final String SAMPLE_STREAM_ID = "1";
+    public static final String TAG = MainActivity.class.getCanonicalName();
 
-    private ExoPlayer exoPlayer;
+    public static final String SAMPLE_ACCOUNT_ID = "81";
+    public static final String SAMPLE_CHANNEL_ID = "300";
+    public static final String SAMPLE_STREAM_ID = "300";
+
+    private PlayerView exoplayerView = null;
+    private SimpleExoPlayer exoplayer;
+    private MediaSessionCompat mediaSession;
+    private ConcatenatingMediaSource concatenatingMediaSource;
+    private  PlaybackStateCompat.Builder playbackStateBuilder;
+
     private OverlayManager overlayManager;
+    private OverlayView overlayView;
 
+    private Boolean fullscreen = false;
+    private ImageView fullscreenButton = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        exoplayerView = findViewById(R.id.simple_exo_player_view);
+        fullscreenButton = findViewById(R.id.exo_fullscreen_icon);
+        overlayView = findViewById(R.id.wisdk_overlay_view);
 
         init(savedInstanceState);
     }
@@ -44,7 +83,83 @@ public class MainActivity extends AppCompatActivity {
 
     private void initializePlayer() {
         //init exoplayer 0, 1 , 2 , 3 ...
+        concatenatingMediaSource = new ConcatenatingMediaSource();
+        ComponentName componentName = new ComponentName(getBaseContext(), "Exo");
 
+        exoplayer = new SimpleExoPlayer.Builder(getBaseContext()).build();
+        exoplayerView.setPlayer(exoplayer);
+        exoplayerView.setUseController(true);
+
+        playbackStateBuilder = new PlaybackStateCompat.Builder();
+        playbackStateBuilder.setActions(PlaybackStateCompat.ACTION_PLAY |
+                PlaybackStateCompat.ACTION_PAUSE |
+                PlaybackStateCompat.ACTION_FAST_FORWARD);
+
+        mediaSession = new MediaSessionCompat(getBaseContext(), "ExoPlayer", componentName, null);
+        mediaSession.setPlaybackState(playbackStateBuilder.build());
+        mediaSession.setActive(true);
+
+
+        fullscreenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(fullscreen) {
+                    fullscreenButton.setImageDrawable(
+                            ContextCompat.getDrawable(
+                                    getBaseContext(),
+                    R.drawable.ic_fullscreen_open
+                        )
+                    );
+
+                    getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_VISIBLE);
+                    if(getSupportActionBar() != null) {
+                        getSupportActionBar().show();
+                    }
+
+                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+                    //player
+                    ConstraintLayout.LayoutParams params = ( ConstraintLayout.LayoutParams) exoplayerView.getLayoutParams();
+                    params.width = ViewGroup.LayoutParams.MATCH_PARENT;
+                    params.height = (int) (250 * getApplicationContext().getResources()
+                            .getDisplayMetrics().density);
+                    exoplayerView.setLayoutParams(params);
+
+                    //overlays
+                    overlayView.setLayoutParams(params);
+
+                    fullscreen = false;
+                } else {
+                    fullscreenButton.setImageDrawable(
+                            ContextCompat.getDrawable(
+                                    getBaseContext(),
+                                    R.drawable.ic_fullscreen_close
+                            )
+                    );
+
+                    getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+
+                    if(getSupportActionBar() != null) {
+                        getSupportActionBar().hide();
+                    }
+
+                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+                    //player
+                    ConstraintLayout.LayoutParams params = ( ConstraintLayout.LayoutParams) exoplayerView.getLayoutParams();
+                    params.width = ViewGroup.LayoutParams.MATCH_PARENT;
+                    params.height = ViewGroup.LayoutParams.MATCH_PARENT;
+                    exoplayerView.setLayoutParams(params);
+
+                    //overlays
+                    overlayView.setLayoutParams(params);
+
+                    fullscreen = true;
+                }
+            }
+        });
     }
 
     private void initializeOverlays() {
@@ -61,12 +176,17 @@ public class MainActivity extends AppCompatActivity {
 
         overlayManager.addUserPlayerListener(new UserActionListener() {
             @Override
-            public void onVoted(@NotNull String userId, @NotNull String channelId, @Nullable String streamId, @NotNull String entryId, int numPredictSame) {
+            public void onVoted(@NotNull String s, @NotNull String s1, @Nullable String s2, @NotNull String s3, @NotNull String s4, @NotNull String s5, @NotNull String s6, int i) {
 
             }
 
             @Override
-            public void onUserPurchase(@NotNull String userId, int productId) {
+            public void onUserPurchase(@NotNull String s, @NotNull String s1) {
+
+            }
+
+            @Override
+            public void onTokenExpire() {
 
             }
         });
@@ -99,9 +219,21 @@ public class MainActivity extends AppCompatActivity {
             }
 
             @Override
-            public void onConfigReady(@NotNull ConfigData configData) {
+            public void onConfigReady(@NotNull final ConfigData configData) {
 
                 //ready Overlays
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        for(StreamSource source : configData.getStreamSources()) {
+                            Log.d(TAG, "============onConfigReady: " + source.getUrl() != null ? source.getUrl() : "");
+                            if(exoplayer != null && source.getUrl() != null) {
+                                exoplayer.setPlayWhenReady(true);
+                                exoplayer.prepare(buildMediaSource(source.getUrl()));
+                            }
+                        }
+                    }
+                });
 
 
             }
@@ -113,9 +245,45 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public Long getCurrentPosition() {
 
-                return exoPlayer != null ? exoPlayer.getCurrentPosition() : 0;
+                return exoplayer != null ? exoplayer.getCurrentPosition() : 0L;
             }
         });
+
+        // Add player event listeners to determine overlay visibility.
+        exoplayer.addListener(new Player.EventListener() {
+            @Override
+            public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+                Log.d(TAG, "====onPlayerStateChanged playWhenReady: $playWhenReady - $playbackState");
+
+                if(overlayManager != null)
+                    overlayManager.setVisible(playWhenReady && playbackState == Player.STATE_READY);
+            }
+        });
+    }
+
+    private MediaSource buildMediaSource(String url) {
+        String userAgent = Util.getUserAgent(getApplicationContext(), "Exo");
+        DefaultDataSourceFactory dataSourceFactory = new DefaultDataSourceFactory(getBaseContext(), userAgent);
+        Uri uri = Uri.parse(url);
+        switch (Util.inferContentType(uri)) {
+            case C.TYPE_DASH:
+                return new DashMediaSource
+                        .Factory(dataSourceFactory)
+                        .createMediaSource(uri);
+            case C.TYPE_HLS:
+                return new HlsMediaSource
+                        .Factory(dataSourceFactory)
+                        .setAllowChunklessPreparation(true)
+                        .createMediaSource(uri);
+            case C.TYPE_SS:
+                return new SsMediaSource
+                        .Factory(dataSourceFactory)
+                        .createMediaSource(uri);
+            case C.TYPE_OTHER:
+                return new ProgressiveMediaSource.Factory(dataSourceFactory)
+                        .createMediaSource(uri);
+        }
+        return null;
     }
 
     @Override
